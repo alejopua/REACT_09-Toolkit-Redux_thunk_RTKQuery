7cc4abe2a4c543c6de1ab84e59855a6d2647fb12
alejopua <alejopua@gmail.com>
2024-04-29 16:06:18 -0500

    Update counterSlice reducer in store.js

--- src/store/slicer/counter/counterSlice.js
+++ src/store/slicer/counter/counterSlice.js
@@ -6,15 +6,22 @@ export const counterSlice = createSlice({
     counter: 10,
   },
   reducers: {
-    increment: (state) => {
+    decrement: (state) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
+      state.counter--;
+    },
+    incrementByAmount: (state, action) => {
+      const plus = action.payload;
+      state.counter += plus;
+    },
+    increment: (state) => {
       state.counter++;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
