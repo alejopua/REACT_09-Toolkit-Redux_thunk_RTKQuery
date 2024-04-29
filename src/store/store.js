414cf5c85110850d9b6f9bff01b507d59b81db95
alejopua <alejopua@gmail.com>
2024-04-29 16:49:36 -0500

    Add pokemonSlice reducer to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./slicer/counter/counterSlice";
+import { pokemonSlice } from "./slicer/pokemon/pokemonSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
+    pokemon: pokemonSlice.reducer,
   },
 });
