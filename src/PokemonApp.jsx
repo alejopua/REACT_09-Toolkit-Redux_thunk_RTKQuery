0fdf9b5af009f43b78fd2ddc8445b96a927e0d83
alejopua <alejopua@gmail.com>
2024-04-29 23:29:06 -0500

    Update PokemonApp to display loading state and list of pokemons from store in src/PokemonApp.jsx

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slicer/pokemon/thunks";
 
 export const PokemonApp = () => {
+  const { pokemons, page, isLoading } = useSelector((state) => state.pokemon);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getPokemons());
   }, []);
@@ -14,10 +16,14 @@ export const PokemonApp = () => {
         <h1>PokemonApp</h1>
         <hr />
         <ul>
-          <li>hola</li>
-          <li>hola</li>
-          <li>hola</li>
+          {isLoading && <p>Loading...</p>}
+          {pokemons.map((pokemon, index) => (
+            <li key={index}>{pokemon.name}</li>
+          ))}
         </ul>
+        <div className="containerButtons">
+          <button onClick={() => dispatch(getPokemons(page))}>Next</button>
+        </div>
       </div>
     </>
   );
