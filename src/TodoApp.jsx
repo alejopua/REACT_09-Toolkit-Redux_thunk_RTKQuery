e665d8b976be455638108e64737539a1778902b3
alejopua <alejopua@gmail.com>
2024-04-30 11:13:33 -0500

    Refactor TodoApp component to include loading state and display todos from API in src/TodoApp.jsx

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,10 +1,25 @@
+import { useGetTodosQuery } from "./store/api/todosApi";
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? "True" : "False"}.</h4>
       <pre>...</pre>
+      <ul>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <li>
+              {" "}
+              <strong>{todo.completed ? "Done: " : "Pending: "}</strong>
+              {todo.title}
+            </li>
+          </div>
+        ))}
+      </ul>
       <button>Next Todo</button>
     </>
   );
