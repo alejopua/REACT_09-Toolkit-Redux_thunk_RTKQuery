2ed180d422c80dac97962d1927ac97b75e782737
alejopua <alejopua@gmail.com>
2024-04-29 21:53:19 -0500

    Fetch and log data from API in getPokemons thunk

--- src/store/slicer/pokemon/thunks.js
+++ src/store/slicer/pokemon/thunks.js
@@ -4,6 +4,11 @@ export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
     // todos: realizar la petición a la API
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    );
+    const data = await response.json();
+    console.log(data);
 
     // dispatch( setPokemons( ))
   };
