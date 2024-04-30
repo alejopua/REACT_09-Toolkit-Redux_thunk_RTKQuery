d653b4f55e11b214e5dacae8895d742a5bba9c1b
alejopua <alejopua@gmail.com>
2024-04-30 10:43:50 -0500

    Add todosApi module for fetching todos from API in src/store/api/todosApi.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./slicer/counter/counterSlice";
 import { pokemonSlice } from "./slicer/pokemon/pokemonSlice";
+import { todosApi } from "./api/todosApi";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemon: pokemonSlice.reducer,
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
